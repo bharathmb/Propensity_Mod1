a8ebd36ecbc052468fc3970ac82ec85a5f94367d
bharathmb <42334605+bharathmb@users.noreply.github.com>
2018-08-31 17:18:50 +0530

    Update read_df.js

--- inst/www/read_df.js
+++ inst/www/read_df.js
@@ -32,12 +32,42 @@ $(document).ready(function(){
       df_full : mydata
     }, function(session){
       session.getConsole(function(output){
-        $("#output code").text(output);
-      });
+        $("#output code").text(output);		
+     
     }).fail(function(){
       alert("Server error: " + req.responseText);
     });        
+  });
   }
+  
+  
+		//Adding code for var_list call
+	
+		function add_var_list()
+			{
+				alert("inside Add Options");
+				var vars;
+				var req = ocpu.call("imp_var_list", {
+				"target.var.name" : "diabeties"
+			}, 
+			function(session){
+				session.getObject(function(data){
+				$("#output code").text(data);	
+				// vars = $("#output").text(outtxt); 
+				//for (var i=0; i < data.length;++i){
+				//addOption(document.drop_list.DropList, data[i], data[i]);
+				}
+			});	
+			alert("imp_var_list equals: " );
+			);
+				req.fail(function(){
+					alert("R returned an error in var_list: " + req.responseText);
+					});
+				
+				};
+				
+	
+		//VAR LIST CALL ENDS
   
   $("#submitbutton").on("click", function(){
     
@@ -53,33 +83,7 @@ $(document).ready(function(){
     
     uploadcsv(filename);        
   });
-  
-  
- function addOption(selectbox,text,value )
-	{
-		var optn = document.createElement("OPTION");
-		optn.text = text;
-		optn.value = value;
-		selectbox.options.add(optn);
-	}
-	
-	$("#addOption_list").on("click",function (){
-	 alert("inside Add Options");
-	 var vars;
-	 var req = ocpu.call("imp_var_list", {
-       "target.var.name" : "diabeties"
-   }, function(session){
-       session.getObject(function(data){
-         // vars = $("#output").text(outtxt); 
-		  for (var i=0; i < data.length;++i){
-			addOption(document.drop_list.DropList, data[i], data[i]);
-			}
-       });
-		alert("imp_var_list equals: " );
-   });
-	req.fail(function(){
-		alert("R returned an error: " + req.responseText);
-		});
-	
-	});
-});
+
+
+  });
+
