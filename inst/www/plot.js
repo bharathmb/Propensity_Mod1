e96d07529b362784dcf69d1b147046283798712d
bharathmb <42334605+bharathmb@users.noreply.github.com>
2018-09-03 17:04:31 +0530

    Update plot.js

--- inst/www/plot.js
+++ inst/www/plot.js
@@ -4,15 +4,19 @@ $(document).ready(function(){
   
   $("#submitbutton").on("click", function(){
     
-	alert("inside plot script....");
+	alert("inside up plot script....");
 
 	
 	    //var nfield = parseInt($("#nfield").val());
         //var distfield = $("#distfield").val();
-        
+        nfield=1000
+	 distfield="normal"
         //create the plot area on the plotdiv element
 	  alert("plotting");
-        var req = $("#plotdiv").rplot("randomplot")
+                var req = $("#plotdiv").rplot("randomplot", {
+          n : nfield,
+          dist : distfield
+        })
 
         //if R returns an error, alert the error message
         req.fail(function(){
