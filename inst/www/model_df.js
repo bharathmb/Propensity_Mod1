fcd9991dbb3f50d18c30b73c65a4ff8ebc456702
bharathmb <42334605+bharathmb@users.noreply.github.com>
2018-09-04 17:45:25 +0530

    Update model_df.js

--- inst/www/model_df.js
+++ inst/www/model_df.js
@@ -8,13 +8,38 @@ $(document).ready(function(){
 	//Check which model is selected
 	var dvname=$("#dvname").val()
 	
-	var isChecked = $('#select-1').prop('checked');
-	  alert(isChecked);
+	var isChecked=""
+	
+	if($('#select-1').prop('checked')==TRUE)
+		{
+		 isChecked="select-1"
+		} else if($('#select-2').prop('checked')==TRUE)
+		{
+		 isChecked="select-2"
+		} else if($('#select-3').prop('checked')==TRUE)
+		{
+		 isChecked="select-3"
+		} else if($('#select-4').prop('checked')==TRUE)
+		{
+		 isChecked="select-4"
+		} else if($('#select-5').prop('checked')==TRUE)
+		{
+		 isChecked="select-5"
+		} else if($('#select-6').prop('checked')==TRUE)
+		{
+		 isChecked="select-6"
+		}
+		else if($('#select-7').prop('checked')==TRUE)
+		{
+		 isChecked="select-7"
+		}
+		
+		alert(isChecked);
 
 		
     //perform the request
     var req = ocpu.call("modelling_module", {
-      "DV" : dvname, "model_selection" :  "select-1"
+      "DV" : dvname, "model_selection" :  isChecked
     }, function(session){
 		//get results and display
 		alert("Model Done");
@@ -31,7 +56,5 @@ $(document).ready(function(){
     });   
 
   });
-
-
   });
 
