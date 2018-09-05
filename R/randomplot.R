e61e65eadd134bf1d57dd235a79e0318a2b5a37f
bharathmb <42334605+bharathmb@users.noreply.github.com>
2018-09-05 11:45:36 +0530

    Update randomplot.R

--- R/randomplot.R
+++ R/randomplot.R
@@ -5,8 +5,8 @@
 #' @export
 #' @param n numer of random values 
 #' @param dist one of "normal" or "uniform".
-randomplot <- function(nfield,distfield){
+randomplot <- function(nfield,distfield,title){
   #input validation
-    hist(rnorm(1000))
+    hist(rnorm(1000),main=title)
   invisible();  
 }
