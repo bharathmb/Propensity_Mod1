caeafebe5aefbc1545b55a44ec89cdc495f9a06d
bharathmb <42334605+bharathmb@users.noreply.github.com>
2018-09-06 18:37:05 +0530

    Update printsummary.R

--- R/printsummary.R
+++ R/printsummary.R
@@ -295,7 +295,7 @@ return(final_data_after_processing)
   
     ##Calling the Data Cleaning function HERE!!
    #df_full<-clean_df(df_full);
-    write.csv(df_full,"C:/opencpuapp_ip/cleaned_data");
+    write.csv(df_full,"C:/opencpuapp_ip/cleaned_data.csv");
 	
   ## 80% of the sample size
   smp_size <- floor(0.8 * nrow(df_full))
