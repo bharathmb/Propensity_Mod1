65b0e62cff2b49a6ceeb9631f284e28b0bb7fa76
bharathmb <42334605+bharathmb@users.noreply.github.com>
2018-09-07 11:26:28 +0530

    Update variable_profiling_function.R

--- R/variable_profiling_function.R
+++ R/variable_profiling_function.R
@@ -1,13 +1,14 @@
-
 variable_profiling_function  <- function(dv, var) {
-	
-	dat = read.csv("c:/opencpuapp_ip/data_after_binning.csv")
-	
-	drops <- c("X")
-	dat<-dat[ , !(names(dat) %in% drops)]
+  
+  library(ggplot2)
+  
+  dat = read.csv("c:/opencpuapp_ip/data_after_binning.csv")
+  
+  drops <- c("X")
+  dat<-dat[ , !(names(dat) %in% drops)]
   
   ##TRAIN & TEST SPLIT
-    ## 80% of the sample size
+  ## 80% of the sample size
   smp_size <- floor(0.8 * nrow(dat))
   
   ## set the seed to make your partition reproducible
@@ -20,25 +21,25 @@ variable_profiling_function  <- function(dv, var) {
   write.csv(train_from_user,"C:/opencpuapp_ip/train_comp.csv");
   write.csv(test_from_user,"C:/opencpuapp_ip/test_comp.csv");
   
-var1 = dat[,var]
-dv = dat[,dv]
+  var1 = dat[,var]
+  dv = dat[,dv]
   
   freq <- table(var1,dv)
   total <- freq[,"0"]+freq[,"1"]
   meaniv = freq[,"1"]/total
   ggplot(dat, aes(var1, fill=dv)) + geom_bar(stat="count",position=position_dodge()) +
-  #p <- ggplot(diab_train, aes(npreg, fill=diabeties)) + geom_bar(stat="count",position=position_dodge()) + 
- # scale_fill_manual(values=c("#999999", "#E69F00")) + 
-              labs(title = "Bivariate Analysis") + 
-  geom_hline(aes(yintercept = mean(meaniv)*100)) +
-  theme(
-  panel.background = element_rect(fill = "aliceblue",
-                                  colour = "lightblue",
-                                  size = 0.5),
-  panel.grid.major = element_line(size = 0.5, linetype = 'solid',
-                                  colour = "white"), 
-  panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                  colour = "lightblue")
-  )
+    #p <- ggplot(diab_train, aes(npreg, fill=diabeties)) + geom_bar(stat="count",position=position_dodge()) + 
+    # scale_fill_manual(values=c("#999999", "#E69F00")) + 
+    labs(title = "Bivariate Analysis") + 
+    geom_hline(aes(yintercept = mean(meaniv)*100)) +
+    theme(
+      panel.background = element_rect(fill = "aliceblue",
+                                      colour = "lightblue",
+                                      size = 0.5),
+      panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                      colour = "white"), 
+      panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                      colour = "lightblue")
+    )
   
 }
