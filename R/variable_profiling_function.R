c4abac5b893e5a008d7eac2e1859778aadc23023
bharathmb <42334605+bharathmb@users.noreply.github.com>
2018-09-07 13:12:18 +0530

    Update variable_profiling_function.R

--- R/variable_profiling_function.R
+++ R/variable_profiling_function.R
@@ -27,19 +27,19 @@ variable_profiling_function  <- function(dv, var) {
   freq <- table(var1,dv)
   total <- freq[,"0"]+freq[,"1"]
   meaniv = freq[,"1"]/total
-  ggplot(dat, aes(var1, fill=dv)) + geom_bar(stat="count",position=position_dodge()) +
-    #p <- ggplot(diab_train, aes(npreg, fill=diabeties)) + geom_bar(stat="count",position=position_dodge()) + 
-    # scale_fill_manual(values=c("#999999", "#E69F00")) + 
-    labs(title = "Bivariate Analysis") + 
-    geom_hline(aes(yintercept = mean(meaniv)*100)) +
-    theme(
-      panel.background = element_rect(fill = "aliceblue",
-                                      colour = "lightblue",
-                                      size = 0.5),
-      panel.grid.major = element_line(size = 0.5, linetype = 'solid',
-                                      colour = "white"), 
-      panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                      colour = "lightblue")
-    )
+  p <- ggplot(dat, aes(var1, fill=dv)) + geom_bar(stat="count",position=position_dodge()) +
+  #p <- ggplot(diab_train, aes(npreg, fill=diabeties)) + geom_bar(stat="count",position=position_dodge()) + 
+  scale_fill_manual(values=c("#56B4E9", "#FF9999")) + 
+              labs(title = "Bivariate Analysis") + 
+  theme(
+  panel.background = element_rect(fill = "aliceblue",
+                                  colour = "lightblue",
+                                  size = 0.5),
+  panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                  colour = "white"), 
+  panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                  colour = "lightblue")
+  )
+  p + geom_hline(aes(yintercept = mean(meaniv)*100, color = "red"))
   
 }
